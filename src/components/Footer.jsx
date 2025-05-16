af45d4a9fc2edb264339bad97bc68b512e0a9f7e
Aashish sharma <aashish12829@gmail.com>
2025-05-16 19:00:04 +0530

    last commint

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-     <footer className="bg-gray-800 text-white text-center py-4">
+     <footer className="bg-[#ffff00] text-black border-t-10 border-black  text-center py-4">
       <p>&copy; {new Date().getFullYear()} Aashish Sharma. All rights reserved.</p>
     </footer>
   )
